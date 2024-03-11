e4c0ea8031772b02756a89d992ef9a70bbbb2565
Riya Kumari <59320441+riya-kumari@users.noreply.github.com>
2024-03-11 18:07:54 -0400

    Add files via upload

---
+++ cirq_helper.py
@@ -0,0 +1,54 @@
+import tensorflow as tf
+import tensorflow_quantum as tfq
+
+import gym, cirq, sympy
+import numpy as np
+from functools import reduce
+from collections import deque, defaultdict
+import matplotlib.pyplot as plt
+from cirq.contrib.svg import SVGCircuit
+tf.get_logger().setLevel('ERROR')
+
+def one_qubit_rotation(qubit, symbols):
+    """
+    Returns Cirq gates that apply a rotation of the bloch sphere about the X,
+    Y and Z axis, specified by the values in `symbols`.
+    """
+    return [cirq.rx(symbols[0])(qubit),
+            cirq.ry(symbols[1])(qubit),
+            cirq.rz(symbols[2])(qubit)]
+
+def entangling_layer(qubits):
+    """
+    Returns a layer of CZ entangling gates on `qubits` (arranged in a circular topology).
+    """
+    cz_ops = [cirq.CZ(q0, q1) for q0, q1 in zip(qubits, qubits[1:])]
+    cz_ops += ([cirq.CZ(qubits[0], qubits[-1])] if len(qubits) != 2 else [])
+    return cz_ops
+
+def generate_circuit(qubits, n_layers):
+    """Prepares a data re-uploading circuit on `qubits` with `n_layers` layers."""
+    # Number of qubits
+    n_qubits = len(qubits)
+
+    # Sympy symbols for variational angles
+    params = sympy.symbols(f'theta(0:{3*(n_layers+1)*n_qubits})')
+    params = np.asarray(params).reshape((n_layers + 1, n_qubits, 3))
+
+    # Sympy symbols for encoding angles
+    inputs = sympy.symbols(f'x(0:{n_layers})'+f'_(0:{n_qubits})')
+    inputs = np.asarray(inputs).reshape((n_layers, n_qubits))
+
+    # Define circuit
+    circuit = cirq.Circuit()
+    for l in range(n_layers):
+        # Variational layer
+        circuit += cirq.Circuit(one_qubit_rotation(q, params[l, i]) for i, q in enumerate(qubits))
+        circuit += entangling_layer(qubits)
+        # Encoding layer
+        circuit += cirq.Circuit(cirq.rx(inputs[l, i])(q) for i, q in enumerate(qubits))
+
+    # Last varitional layer
+    circuit += cirq.Circuit(one_qubit_rotation(q, params[n_layers, i]) for i,q in enumerate(qubits))
+
+    return circuit, list(params.flat), list(inputs.flat)
